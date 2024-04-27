d3e205551f145e71fb5ccd213bdb1e0a8b050fd1
TheKingsident <kingsident@gmail.com>
2024-04-27 18:27:46 +0100

    Updated

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,38 +1,29 @@
 -- SQL script that creates a stored procedure ComputeAverageScoreForUser
 -- that computes and store the average score for a student. Note: An average score can be a decimal
+
 DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 
 DELIMITER //
 
-CREATE PROCEDURE ComputeAverageScoreForUser(
-  IN user_id INT
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-  DECLARE total_score DECIMAL(10,2);  -- To store total score with decimals
-  DECLARE num_corrections INT;
-
-  -- Initialize variables
-  SET total_score = 0;
-  SET num_corrections = 0;
-
-  -- Calculate total score and number of corrections for the user
-  SELECT SUM(score), COUNT(*)
-  INTO total_score, num_corrections
-  FROM corrections
-  WHERE user_id = user_id;
-
-  -- Update user's average score (handle division by zero)
-  IF num_corrections > 0 THEN
-    UPDATE users
-    SET average_score = total_score / num_corrections
-    WHERE id = user_id;
-  ELSE
-    -- Set average score to 0 if no corrections found
-    UPDATE users
-    SET average_score = 0
-    WHERE id = user_id;
-  END IF;
-END //
+    DECLARE avg_score DECIMAL(10, 2);
+    DECLARE num_subjects INT;
+    DECLARE score_total DECIMAL(10, 2); -- Changed to DECIMAL
+    
+    -- Compute the average score for the user
+    SELECT SUM(score) INTO score_total FROM corrections WHERE user_id = user_id;
+    SELECT COUNT(*) INTO num_subjects FROM corrections WHERE user_id = user_id;
+    
+    IF num_subjects > 0 THEN
+        SET avg_score = score_total / num_subjects;
+    ELSE
+        SET avg_score = 0;
+    END IF;
+    
+    -- Update the user's average score in the users table
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+END;
+//
 
 DELIMITER ;
-
