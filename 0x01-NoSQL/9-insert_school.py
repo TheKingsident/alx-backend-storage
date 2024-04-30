5d1ddbe839460747b5833b50b77670cc555e0821
TheKingsident <kingsident@gmail.com>
2024-04-30 19:16:11 +0100

    added file for task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Python function that inserts a new document in a collection"""
+
+
+def insert_school(mongo_collection, **kwargs):
+    """function that inserts a new document in a collection"""
+    rlt = mongo_collection.insert_one(kwargs)
+    return rlt.inserted_id
