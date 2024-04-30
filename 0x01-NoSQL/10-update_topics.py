aafea574fc35a38b84d1e4d6f79fc1f166570b67
TheKingsident <kingsident@gmail.com>
2024-04-30 19:41:32 +0100

    added file for task 10

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Python function that changes all topics of a school document"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """function that changes all topics of a school document"""
+    rlt = mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
